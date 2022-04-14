1624deb98198ca5b282324a02acdec4c0862993b
Journey <journesky@gmail.com>
2022-04-15 06:39:42 +0800

    add search view

--- app/src/main/java/tech/jour/ygocdb/module/home/activity/MainActivity.kt
+++ app/src/main/java/tech/jour/ygocdb/module/home/activity/MainActivity.kt
@@ -1,18 +1,21 @@
 package tech.jour.ygocdb.module.home.activity
 
+import android.app.SearchManager
+import android.content.Context
 import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.viewModels
+import androidx.appcompat.widget.SearchView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.google.android.material.snackbar.Snackbar
-import tech.jour.ygocdb.base.ktx.observeLiveData
-import tech.jour.ygocdb.common.ui.BaseActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import dagger.hilt.android.AndroidEntryPoint
 import tech.jour.ygocdb.R
+import tech.jour.ygocdb.base.ktx.observeLiveData
 import tech.jour.ygocdb.base.utils.toast
+import tech.jour.ygocdb.common.ui.BaseActivity
 import tech.jour.ygocdb.databinding.ActivityMainBinding
 
 /**
@@ -24,64 +27,116 @@ import tech.jour.ygocdb.databinding.ActivityMainBinding
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding, HomeViewModel>() {
 
-    /**
-     * 通过 viewModels() + Hilt 获取 ViewModel 实例
-     */
-    override val mViewModel by viewModels<HomeViewModel>()
+	/**
+	 * 通过 viewModels() + Hilt 获取 ViewModel 实例
+	 */
+	override val mViewModel by viewModels<HomeViewModel>()
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
+	private lateinit var appBarConfiguration: AppBarConfiguration
 
-    override fun ActivityMainBinding.initView() {
-        val binding = mBinding
-        setContentView(binding.root)
+	private lateinit var searchView: SearchView
 
-        setSupportActionBar(binding.toolbar)
+	private lateinit var searchMenu: MenuItem
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+	// Get the SearchView and set the searchable configuration
+	private lateinit var searchManager: SearchManager
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-    }
+	override fun ActivityMainBinding.initView() {
+		val binding = mBinding
+		setContentView(binding.root)
 
-    override fun initObserve() {
-        observeLiveData(mViewModel.data, ::processData)
-    }
+		setSupportActionBar(binding.toolbar)
 
-    private fun processData(data: String) {
-        toast(data)
+		searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+
+		val navController = findNavController(R.id.nav_host_fragment_content_main)
+		appBarConfiguration = AppBarConfiguration(navController.graph)
+		setupActionBarWithNavController(navController, appBarConfiguration)
+
+		binding.fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+			showSearch(view as FloatingActionButton)
+		}
+	}
+
+	override fun initObserve() {
+		observeLiveData(mViewModel.data, ::processData)
+	}
+
+	private fun processData(data: String) {
+		toast(data)
 //        mBinding.vTvHello.text = data
 //        mBinding.vTvHello.setTextColor(Color.BLUE)
-    }
-
-    override fun initRequestData() {
-        // 模拟获取数据
-        mViewModel.getData()
-    }
+	}
 
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+	private fun showSearch(fab: FloatingActionButton) {
+		if (searchMenu.isActionViewExpanded) {
+			searchMenu.collapseActionView()
+			fab.setImageResource(R.drawable.ic_baseline_search_24)
+		} else {
+			searchMenu.expandActionView()
+			fab.setImageResource(R.drawable.ic_baseline_send_24)
+		}
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
+	}
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
+	override fun initRequestData() {
+		// 模拟获取数据
+		mViewModel.getData()
+	}
+
+	override fun onCreateOptionsMenu(menu: Menu): Boolean {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		menuInflater.inflate(R.menu.menu_main, menu)
+
+		searchMenu = menu.findItem(R.id.menu_search)
+		searchView = searchMenu.actionView as SearchView
+
+		searchView.apply {
+			// Assumes current activity is the searchable activity
+			setSearchableInfo(searchManager.getSearchableInfo(componentName))
+			setIconifiedByDefault(false) // Do not iconify the widget; expand it by default
+			//搜索框展开时后面叉叉按钮的点击事件
+			setOnCloseListener {
+				toast("Close:关闭")
+				false
+			}
+			//搜索图标按钮(打开搜索框的按钮)的点击事件
+			setOnSearchClickListener {
+				toast("Open:打开")
+			}
+			//搜索框文字变化监听
+			setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+				override fun onQueryTextSubmit(s: String): Boolean {
+					toast("搜索了:$s")
+					return false
+				}
+
+				override fun onQueryTextChange(s: String): Boolean {
+					return false
+				}
+			})
+
+		}
+
+		return true
+	}
+
+	override fun onOptionsItemSelected(item: MenuItem): Boolean {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		return when (item.itemId) {
+			R.id.action_settings -> true
+			else -> super.onOptionsItemSelected(item)
+		}
+	}
+
+	override fun onSupportNavigateUp(): Boolean {
+		val navController = findNavController(R.id.nav_host_fragment_content_main)
+		return navController.navigateUp(appBarConfiguration)
+				|| super.onSupportNavigateUp()
+	}
 }
